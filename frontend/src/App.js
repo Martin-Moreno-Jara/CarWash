5512ce6869ea823592293d1d484d937f5c19d08a
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2024-04-01 23:10:59 -0500

    [feat]: creando navbar y ajustando estilos iniciales css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import FormTest from "./pages/FormTest";
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<h1>hello</h1>} />
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,10 @@
+const NavBar = () => {
+  return (
+    <header>
+      <div className="logo-container">Aqui va el logo</div>
+      <h2>Carwash</h2>
+    </header>
+  );
+};
+
+export default NavBar;

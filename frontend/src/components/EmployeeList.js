77ec4f0f43edefc2aec8d2deb200d86454c97cfc
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2024-04-15 11:37:18 -0500

    [feat]: implementacion inicial con tanstack react table

--- frontend/src/components/EmployeeList.js
+++ frontend/src/components/EmployeeList.js
@@ -1,12 +1,42 @@
+//MODULES IMPORTED
 import { useEffect, useState } from "react";
 import EmployeeInfo from "../components/EmployeeInfo";
 import { useEmployeeContext } from "../hooks/useEmployeeContext";
 import "../stylesheets/EmployeeList.css";
 import MoonLoader from "react-spinners/MoonLoader";
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+} from "@tanstack/react-table";
 import { useEmployeeCrudContext } from "../hooks/useEmployeeCrudContext";
+//ENV VARIABLE API
 const apiURL = process.env.REACT_APP_DEPLOYURL;
 
+//FETCH EMPLOYEES FOR THE TABLE
 const EmployeeList = () => {
+  const columns = [
+    {
+      header: "Nombre",
+      accessorKey: "nombre",
+    },
+    {
+      header: "Usuario",
+      accessorKey: "usuario",
+    },
+    {
+      header: "Teléfono",
+      accessorKey: "telefono",
+    },
+    {
+      header: "Cédula",
+      accessorKey: "cedula",
+    },
+    {
+      header: "Acciones",
+      accessorKey: "acciones",
+    },
+  ];
   const { empleados, dispatch } = useEmployeeContext();
   const { show, showEdit } = useEmployeeCrudContext();
   const [isLoading, setIsLoading] = useState(null);
@@ -29,6 +59,11 @@ const EmployeeList = () => {
     fetchEmployees();
   }, [dispatch]);
 
+  const table = useReactTable({
+    data: empleados,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
   return (
     <div
       className={
@@ -36,29 +71,43 @@ const EmployeeList = () => {
       }
     >
       <table>
-        <tr>
-          <th>Nombre</th>
-          <th>Usuario</th>
-          <th>Teléfono</th>
-          <th>Cédula</th>
-          <th>Acciones</th>
-        </tr>
-
-        {empleados &&
-          empleados.map((empleado) => (
-            <>
-              {
-                <EmployeeInfo
-                  id={empleado._id}
-                  nombre={empleado.nombre}
-                  apellido={empleado.apellido}
-                  usuario={empleado.usuario}
-                  telefono={empleado.telefono}
-                  cedula={empleado.cedula}
-                />
-              }
-            </>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>{header.column.columnDef.header}</th>
+              ))}
+            </tr>
           ))}
+        </thead>
+        <tbody>
+          {empleados &&
+            table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+
+          {empleados &&
+            empleados.map((empleado) => (
+              <>
+                {
+                  <EmployeeInfo
+                    id={empleado._id}
+                    nombre={empleado.nombre}
+                    apellido={empleado.apellido}
+                    usuario={empleado.usuario}
+                    telefono={empleado.telefono}
+                    cedula={empleado.cedula}
+                  />
+                }
+              </>
+            ))}
+        </tbody>
       </table>
       {isLoading && (
         <div className="loading">

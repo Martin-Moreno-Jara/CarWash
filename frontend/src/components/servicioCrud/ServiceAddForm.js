1631434833c0f62815a88b51fae61fb171464e7a
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2024-04-23 10:27:04 -0500

    [feat]: trae objetos de servicios para el admin

--- frontend/src/components/servicioCrud/ServiceAddForm.js
+++ frontend/src/components/servicioCrud/ServiceAddForm.js
@@ -14,6 +14,7 @@ const ServiceAddForm = ({ displaySelf, setDisplay }) => {
   }, [displaySelf]);
 
   const [showFormats, setShowFormats] = useState(false);
+
   return (
     <>
       {display && (
--- frontend/src/pages/CrudServicios.js
+++ frontend/src/pages/CrudServicios.js
@@ -1,16 +1,41 @@
 //************************** IMPORTED
 //REACT HOOKS/IMPORTS
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ServiceAddForm from "../components/servicioCrud/ServiceAddForm";
 //CUSTOM HOOKS
+import { useAuthContext } from "../hooks/useAuthContext";
 //COMPONENTS
 //STYLESHEET
 import "../stylesheets/CrudServicios.css";
 //ENV VARIABLES
+const apiURL = process.env.REACT_APP_DEVURL;
 
 //**************************************************************
 const CrudServicios = () => {
+  const { usuario } = useAuthContext();
+
   const [displayCreate, setDisplayCreate] = useState(false);
+  useEffect(() => {
+    const fetchAllServies = async () => {
+      const response = await fetch(`${apiURL}/api/servicioCRUD`, {
+        headers: { Authorization: `Bearer ${usuario.token}` },
+      });
+      const json = await response.json();
+      if (!response.ok) {
+        throw Error(`no se pudo porque: ${json}`);
+      }
+      if (response.ok) {
+        console.log(json);
+      }
+    };
+    const fetchServicesByEmployee = async () => {};
+    if (usuario.rol === "administrador") {
+      fetchAllServies();
+    }
+    if (usuario.rol === "empleado") {
+      console.log("traer pal empleado");
+    }
+  }, []);
   return (
     <div className="crudServicios-main">
       <div className="empleado-options">

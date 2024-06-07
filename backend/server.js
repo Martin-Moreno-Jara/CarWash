91b600938262fe85d0149cf16945d94b36a17c90
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2024-06-07 14:13:38 -0500

    back funciona

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const servicioCRUDRoutes = require("./routes/servicioCRUD");
 const tarifasRoutes = require("./routes/tarifas");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const reporteRoutes = require("./routes/reporte");
 //const pdf = require("html-pdf");
 
 const app = express();
@@ -27,7 +28,7 @@ app.use("/api/user", userRoutes);
 app.use("/api/empleadoCRUD", empleadoCRUDRoutes);
 app.use("/api/servicioCRUD", servicioCRUDRoutes);
 app.use("/api/tarifas", tarifasRoutes);
-
+app.use("/api/reporte", reporteRoutes);
 
 // db connection
 mongoose
